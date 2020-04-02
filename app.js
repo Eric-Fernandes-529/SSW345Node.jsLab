fcdd612ce6031b268f94f9764ebb104f5a7fa634
Eric-Fernandes-529 <61845397+Eric-Fernandes-529@users.noreply.github.com>
2020-04-02 14:13:26 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const routes = require('./routes/routes');
+const bodyParser = require('body-parser');
+const express = require('express');
+const port = 3002;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+routes(app);
+// Start the server
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+ 
+    console.log(`Server listening on port ${server.address().port}`);
+});
